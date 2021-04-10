7496214bb04e379eb1585eba8016ace3cb54e0a3
apere858 <77993736+apere858@users.noreply.github.com>
2021-04-09 23:36:50 -0400

    Title Screen
    Titlescreen and click to take you to the game

---
+++ Arena.h
@@ -0,0 +1,33 @@
+#ifndef ARENA_H
+#define ARENA_H
+
+class Arena : public wxPanel
+{
+    public:
+        Arena(wxWindow *parent,
+            wxWindowID winid = wxID_ANY,
+            const wxPoint& pos = wxDefaultPosition,
+            const wxSize& size = wxDefaultSize,
+            long style = wxTAB_TRAVERSAL | wxNO_BORDER,
+            const wxString& name = wxPanelNameStr);
+
+        virtual ~Arena();
+
+        int Getintersections() { return intersections; }
+        void Setintersections(int val) { intersections = val; }
+        wxPoint GetbasePosition() { return basePosition; }
+        void SetbasePosition(wxPoint val) { basePosition = val; }
+        int Getlanes() { return lanes; }
+        void Setlanes(int val) { lanes = val; }
+
+    protected:
+
+    private:
+        int intersections;
+        wxPoint basePosition;
+        static int length;
+        static int width;
+        int lanes;
+};
+
+#endif // ARENA_H
